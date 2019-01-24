3d71b2c0f31f425adea4da21d587a067a6b8fd9e
firecoded <yepyepguru@gmail.com>
2019-01-24 14:34:07 -0800

    setup proxy middleware and tested axios call to backend api

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import {Link} from 'react-router-dom';
 import "./landing.css";
 
 export default class Landing extends Component {
   
   
+    componentDidMount = async () => {
+        const resp = await axios.get('/api/test');
+        console.log(resp)
+    }
+    
     render() {
         return (
             <div className = "landing-cont">
