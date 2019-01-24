0d68bc7d9a96ccc2d77866c24478a2c6a015f4c3
firecoded <yepyepguru@gmail.com>
2019-01-23 20:22:10 -0800

    setup basic routing and redux starting files

--- index.js
+++ index.js
@@ -11,26 +11,28 @@ app.use(cors());
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-// app.use(express.static());
+app.use(express.static(resolve(__dirname, 'client', 'dist')));
+
+require('./routes')(app);
 
 app.get('/', (req, res) => {
     res.send('<h1>Server running</h1>')
 })
 
-passport.use(
-    new GoogleStrategy({
-        clientID: keys.googleClientId,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
-    }, (accessToken) => {
-        console.log(accessToken);
-    })
-);
+// passport.use(
+//     new GoogleStrategy({
+//         clientID: keys.googleClientId,
+//         clientSecret: keys.googleClientSecret,
+//         callbackURL: '/auth/google/callback'
+//     }, (accessToken) => {
+//         console.log(accessToken);
+//     })
+// );
 
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-}));
-app.get('/auth/google/callback', passport.authenticate('google'));
+// app.get('/auth/google', passport.authenticate('google', {
+//     scope: ['profile', 'email']
+// }));
+// app.get('/auth/google/callback', passport.authenticate('google'));
 
 
 const PORT = process.env.PORT || 5000;
