f93684e152b340c965224a11a47b1ad8f3c16f32
firecoded <yepyepguru@gmail.com>
2019-01-31 17:30:24 -0800

    created sign in component and connected to redux form

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,8 +4,17 @@ import axios from 'axios';
 export const signUp = userInfo => async dispatch => {
     try{
         const resp = await axios.post('/auth/sign-up', userInfo);
-        console.log("sign up response: ", resp);
+        console.log("redux sign up response: ", resp);
     } catch(err){
-        console.log("sign up error: ", err.message);
+        console.log("redux sign up error: ", err.message);
+    }
+}
+
+export const signIn = userInfo => async dispatch =>{
+    try{
+        const resp = await axios.post('/auth/sign-in', userInfo);
+        console.log("redux sign in response: ", resp);
+    } catch(err){
+        console.log("redux error signing in", err)
     }
 }
--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const { resolve } = require('path');
-// const passport = require('passport');
-// const GoogleStrategy = require('passport-google-oauth20').Strategy;
-// const keys = require('./config/keys');
 
 require('./db');
 
@@ -20,21 +17,6 @@ require('./routes')(app);
 app.get('/', (req, res) => {
     res.send('<h1>Server running</h1>')
 })
-
-// passport.use(
-//     new GoogleStrategy({
-//         clientID: keys.googleClientId,
-//         clientSecret: keys.googleClientSecret,
-//         callbackURL: '/auth/google/callback'
-//     }, (accessToken) => {
-//         console.log(accessToken);
-//     })
-// );
-
-// app.get('/auth/google', passport.authenticate('google', {
-//     scope: ['profile', 'email']
-// }));
-// app.get('/auth/google/callback', passport.authenticate('google'));
 
 
 const PORT = process.env.PORT || 5000;
